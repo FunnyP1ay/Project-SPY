fe399bc645b99d1adf67989c0ce6a2bd3777c0cd
FunnyP1ay <tlstjd042217@gmail.com>
2024-03-13 14:16:27 +0900

    차량의 레이캐스트 생성 및 정차 시스템 수정 중

--- Assets/Scripts/DrivingCar/DrivingCar.cs
+++ Assets/Scripts/DrivingCar/DrivingCar.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -9,26 +10,54 @@ public class DrivingCar : MonoBehaviour
 
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private float raycastRange = 3f;
+    [SerializeField]
+    private LayerMask targetLayer;
 
     public Transform currentDrivingPoint;
 
     private void Start()
     {
         speed = 15f;
+        raycastRange = 5f;
     }
     private void Update()
     {
         if(currentDrivingPoint != null)
         {
-            gameObject.transform.LookAt(currentDrivingPoint);
-            Vector3 moveVec = (currentDrivingPoint.position- transform.position ).normalized;
-            transform.Translate(moveVec * speed * Time.deltaTime,Space.World);
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position,transform.forward, out hit, raycastRange, targetLayer)) 
+            {
+                if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Citizen") ||
+                    hit.collider.gameObject.layer == LayerMask.NameToLayer("DrivingCar"))
+                {
+                    DrivingStop();
+                }
+            }
+            else
+            {
+                Driving();
+            }
         }
-        
-        
+   
+
+
     }
     public void NextDrivingPoint(Transform _nextDrivingPoint)
     {
        currentDrivingPoint = _nextDrivingPoint;
+    }
+
+    private void Driving() 
+    {
+        speed = 50f;
+        gameObject.transform.LookAt(currentDrivingPoint);
+        Vector3 moveVec = (currentDrivingPoint.position - transform.position).normalized;
+        transform.Translate(moveVec * speed * Time.deltaTime, Space.World);
+    }
+    private void DrivingStop()
+    {
+        speed = 0f;
     }
 }
