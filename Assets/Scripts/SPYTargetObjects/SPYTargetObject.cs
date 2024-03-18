a5dda52793c1a074f7d6cd603b3cf511942aef50
FunnyP1ay <tlstjd042217@gmail.com>
2024-03-18 19:36:43 +0900

    URP 컨버팅 성공

--- Assets/Scripts/SPYTargetObjects/SPYTargetObject.cs
+++ Assets/Scripts/SPYTargetObjects/SPYTargetObject.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SPYTargetObject : MonoBehaviour
 {
-    public GameObject   Fkey;
+    public ParticleSystem           particleSystem; // VFX 로 변경 가능
+    public GameObject               Fkey;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.TryGetComponent(out PlayerMove player))
