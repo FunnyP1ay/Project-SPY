0ca0c0aa9716293bef2e923b10d5365318959851
FunnyP1ay <tlstjd042217@gmail.com>
2024-03-22 17:55:23 +0900

    집으로 들어가는 것 완료

---
+++ Assets/Scenes/TestPopUp/PopUpTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpTest : MonoBehaviour
+{
+    public UI_PopUpBase popUp;
+    private void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            popUp.Open(()=>print("popup close"));
+        }
+    }
+}
--- Assets/Scenes/TestPopUp/UI_PopUpBase.cs
+++ Assets/Scenes/TestPopUp/UI_PopUpBase.cs
@@ -1,23 +1,26 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum PopUpResult
-{
-    None,
-    Ok,
-    Cancel
-}
-
 public class UI_PopUpBase : MonoBehaviour
 {
+    public enum PopUpResult
+    {
+        None,
+        Ok,
+        Cancel
+    }
+
     protected Button closeButton;
     protected Action closeCallback;
     private void Start()
     {
         Init();
+        gameObject.SetActive(false);
+        transform.localScale = Vector3.one;
     }
     protected virtual void Awake()
     {
@@ -28,16 +31,17 @@ public class UI_PopUpBase : MonoBehaviour
     {
         closeButton.onClick.AddListener(Close);
     }
-    
+     
     public virtual void Open(Action closeCallback = null) // 
     {
         this.closeCallback = closeCallback;
         gameObject.SetActive(true);
+        transform.DOScale(1f, 0.2f).SetEase(Ease.InBounce);
     }
     public virtual void Close()
     {
         // 창을 닫을 때 수행 할 함수를  정의 
         closeCallback.Invoke();
-        gameObject.SetActive(false);
+        transform.DOScale(0f, 0.2f).SetEase(Ease.InBounce).onComplete += () => gameObject.SetActive(false);
     }
 }
