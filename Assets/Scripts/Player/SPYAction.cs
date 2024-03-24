4a62f4615b513b848abdfa305fd3820941b8d468
FunnyP1ay <tlstjd042217@gmail.com>
2024-03-24 22:03:12 +0900

    플레이어가 경찰에 추적 당하는 시스템 완료

--- Assets/Scripts/Player/SPYAction.cs
+++ Assets/Scripts/Player/SPYAction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SPYAction : MonoBehaviour
 {
+    private Coroutine needChageCoatCoroutine=null;
     public void BrokenObjectAttack(SPYTargetObject _target)
     {
         _target.OnAttack();
@@ -38,12 +39,24 @@ public class SPYAction : MonoBehaviour
         {
             UI_Manager.Instance.ui_Player_Coat_Icon.gameObject.SetActive(true);
             UI_Manager.Instance.ui_Player_Coat_Icon.enabled = true;
-
+            if(needChageCoatCoroutine == null)
+            needChageCoatCoroutine = StartCoroutine(NeedChageCoat());
         }
         else
         {
             UI_Manager.Instance.ui_Player_Coat_Icon.gameObject.SetActive(false);
             UI_Manager.Instance.ui_Player_Coat_Icon.enabled = false;
+            StopCoroutine(NeedChageCoat());
+            needChageCoatCoroutine = null;
+        }
+    }
+    public IEnumerator NeedChageCoat()
+    {
+        while (true)
+        {
+            //바로    ExposedAction(); 을 호출하면 스택오버 플로우 발생하므로 제일 조심
+            yield return new WaitForSecondsRealtime(1f);
+            ExposedAction(10f);
         }
     }
 }
