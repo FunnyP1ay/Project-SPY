bcd1f86c6ed062e823bbf48cab2e8f2ed4cb4b74
FunnyP1ay <tlstjd042217@gmail.com>
2024-04-12 14:18:16 +0900

    건물 부셔지는 효과 추가 중

--- Assets/Scripts/Building/Building.cs
+++ Assets/Scripts/Building/Building.cs
@@ -2,11 +2,13 @@ using Lean.Pool;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class Building : MonoBehaviour
 {
-    public float                HP = 100f;
+    public float                HP = 200f;
     public Transform            building_NavTargetPoint;
+    public List<VisualEffect>   vfx_smoke_Lsit;
     public void Setting()
     {
         foreach (Transform child in transform)
@@ -19,11 +21,26 @@ public class Building : MonoBehaviour
     public void GetDamage(float _damage)
     {
         HP -= _damage;
+        foreach(VisualEffect vfx in vfx_smoke_Lsit)
+        {
+            vfx.gameObject.SetActive(true);
+            vfx.Play();
+        }
         if(HP <= 0)
         {
-            print("건물이 부셔졌습니다 ! ");
-            LeanPool.Despawn(this.gameObject);
-            // 건물 부셔지는거 구현 하기
+            Destroy();
         }
+    }
+    public void Destroy()
+    {
+        print("건물이 부셔졌습니다 ! ");
+        CityControlData.Instance.safety_Rating -= 1f;
+        foreach (VisualEffect vfx in vfx_smoke_Lsit)
+        {
+            vfx.gameObject.SetActive(false);
+            vfx.Stop();
+        }
+        LeanPool.Despawn(this.gameObject);
+        // 건물 부셔지는거 구현 하기
     }
 }
